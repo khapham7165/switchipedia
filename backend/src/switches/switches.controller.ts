8db5698060be0860abd440b10d185d3d978e3105
khapk <khapk@vng.com.vn>
2022-11-06 02:11:39 +0700

    get by id api

--- backend/src/switches/switches.controller.ts
+++ backend/src/switches/switches.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 import { SwitchModel } from 'src/mongo/models'
 
-@Controller('switches')
+@Controller('switch')
 export class SwitchesController {
   @Get('/all')
   findAll(): any {
-    return SwitchModel.find()
+    return SwitchModel.find().populate(['manufacturer', 'brand', 'switchType'])
+  }
+
+  @Get(':id')
+  findOne(@Param() params): any {
+    return SwitchModel.findById(params.id).populate([
+      'manufacturer',
+      'brand',
+      'switchType',
+    ])
   }
 }
