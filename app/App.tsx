d66732af25e28f0de4fbc1376678c4e12c50d88f
khapk <khapk@vng.com.vn>
2022-11-12 16:44:26 +0700

    add antd mobile comps

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button, Space } from 'antd-mobile'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button style={styles.button} color="primary">
+        Ant btn
+      </Button>
       <StatusBar style="auto" />
     </View>
   )
@@ -17,5 +21,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flex: 1,
     justifyContent: 'center',
+  },
+  button: {
+    borderRadius: 10,
+    backgroundColor: 'orange',
   },
 })
